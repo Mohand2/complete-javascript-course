f2fd153b6751327859fc6c9821e42e4e4ed85408
Mohand2 <alloun.mohand@gmail.com>
2022-07-11 11:31:45 +0100

    challenge 01

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,19 @@
+const markMass = 78;
+const johnMass = 92;
+
+const markHeight = 1.69;
+const johnHeight = 1.95;
+
+
+let markbmi;
+let johnbmi;
+
+    
+markbmi = markMass / markHeight ** 2;
+johnbmi = johnMass / johnHeight ** 2;
+
+let markHigherBmi = markbmi > 24;
+
+console.log("Mark bmi: " + markbmi);
+console.log("John bmi: " + johnbmi);
+console.log(markHigherBmi);
