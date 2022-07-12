efdb53bcd903ee06bd4497fcf4f14b6342d49561
Mohand2 <alloun.mohand@gmail.com>
2022-07-12 16:35:08 +0100

    added code

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,19 +1,34 @@
-const markMass = 78;
-const johnMass = 92;
-
-const markHeight = 1.69;
-const johnHeight = 1.95;
 
 
-let markbmi;
-let johnbmi;
+
+let birth;
+const now = 2022;
+birth = Number(prompt('Your date of birth: '));
+console.log(`your age is: ${now - birth}`);
+
+
+
+
+
+
+
+// const markMass = 78;
+// const johnMass = 92;
+
+// const markHeight = 1.69;
+// const johnHeight = 1.95;
+
+
+// let markbmi;
+// let johnbmi;
 
     
-markbmi = markMass / markHeight ** 2;
-johnbmi = johnMass / johnHeight ** 2;
+// markbmi = markMass / markHeight ** 2;
+// johnbmi = johnMass / johnHeight ** 2;
 
-let markHigherBmi = markbmi > 24;
+// let markHigherBmi = markbmi > 24;
 
-console.log("Mark bmi: " + markbmi);
-console.log("John bmi: " + johnbmi);
-console.log(markHigherBmi);
+// console.log("Mark bmi: " + markbmi);
+// console.log("John bmi: " + johnbmi);
+// console.log(markHigherBmi);
+ 
